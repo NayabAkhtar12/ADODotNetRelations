17cac4d38af0af488c352e9938ac3035ab1d91ff
NayabAkhtar12 <119120061+NayabAkhtar12@users.noreply.github.com>
2023-01-23 22:31:28 +0500

    Completed!

--- ADODotNetRelations/Program.cs
+++ ADODotNetRelations/Program.cs
@@ -1,13 +1,15 @@
-﻿using System.Data.SqlClient;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
 namespace ADODotNetRelations
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string connectionstring = "Data source=DESKTOP-FH244E0\\SQLEXPRESS; Database=ConnectToSql; Integrated Security=SSPI;";
-            SqlConnection connection = new SqlConnection(connectionstring);
-            var sqlcommand = new SqlCommand("select * from Customers", connection);
+            string connectionString = "Data source=DESKTOP-FH244E0\\SQLEXPRESS; Database=User; Integrated Security=SSPI;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            var sqlcommand = new SqlCommand("select * from UserInfo", connection);
             connection.Open();
             using (SqlDataReader reader = sqlcommand.ExecuteReader())
             {
@@ -17,26 +19,26 @@ namespace ADODotNetRelations
 
                 }
             }
-          //Insert Operation
-            string InsertCustomerQuery = "INSERT INTO Customers (CustomerID, CustomerName) VALUES(@CustomerID,@CustomerName)";
-            SqlCommand Insertcommad = new SqlCommand(InsertCustomerQuery,connection);
-            Insertcommad.Parameters.AddWithValue("@CustomerID", 3);
-            Insertcommad.Parameters.AddWithValue("@CustomerName", "Nayab");
-            int recordsadded=Insertcommad.ExecuteNonQuery();
+            ////Insert Operation
+            string InsertCustomerQuery = "INSERT INTO UserInfo(Id, Name) VALUES(@Id,@Name)";
+            SqlCommand Insertcommad = new SqlCommand(InsertCustomerQuery, connection);
+            Insertcommad.Parameters.AddWithValue("@Id", 3);
+            Insertcommad.Parameters.AddWithValue("@Name", "Nayab");
+            int recordsadded = Insertcommad.ExecuteNonQuery();
             Console.WriteLine($"{recordsadded} no of records added");
 
-            //Update Operation
-            string UpdateQuery = "UPDATE Customers SET CustomerName=@CustomerName WHERE CustomerID=@CustomerID";
+           // Update Operation
+            string UpdateQuery = "UPDATE UserInfo SET Name=@Name WHERE Id=@Id";
             SqlCommand Updatecommad = new SqlCommand(UpdateQuery, connection);
-            Updatecommad.Parameters.AddWithValue("@CustomerName", "AYESHA");
-            Updatecommad.Parameters.AddWithValue("@CustomerID", 3);
+            Updatecommad.Parameters.AddWithValue("@Name", "Laiba");
+            Updatecommad.Parameters.AddWithValue("@Id", 3);
             int recordupdated = Updatecommad.ExecuteNonQuery();
             Console.WriteLine($"{recordupdated} no of records Updated");
 
             //Delete Operation
-            string Deletequery = "DELETE FROM  Customers WHERE CustomerID=@CustomerID";
+            string Deletequery = "DELETE FROM  UserInfo WHERE Id=@Id";
             SqlCommand Deletecommad = new SqlCommand(Deletequery, connection);
-            Deletecommad.Parameters.AddWithValue("@CustomerID", 3);
+            Deletecommad.Parameters.AddWithValue("@Id", 3);
             int recorddeleted = Deletecommad.ExecuteNonQuery();
             Console.WriteLine($"{recorddeleted} no of records Deleted");
             Console.WriteLine("Program ends here");
